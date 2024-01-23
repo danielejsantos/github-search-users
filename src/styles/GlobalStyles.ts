bef8fda2869dbf1fd5e211fe69e828171536abdd
danielejsantos <danijovina@gmail.com>
2024-01-23 15:41:26 -0300

    feat: creates global styles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  html, body, #root {
+    height: 100%;
+  }
+  *, button, input {
+    border: 0;
+    font-family: 'Robot', sans-serif;
+    outline: 0;
+  }
+`;
